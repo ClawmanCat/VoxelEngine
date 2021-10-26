a082d6d4e5813975608cd088851f709c6f6f6583
ClawmanCat <clawmancat@gmail.com>
2021-10-26 16:16:34 +0200

    Remove old files

--- out_dirs/assets/shaders/wip/pbr_multi_pass.g_input.glsl
+++
@@ -1,8 +0,0 @@
-#version 430
-#include "multipass.util.glsl"
-
-
-void main() {
-    // Generate a screen-filling quad to render to.
-    gl_Position = vec4(quad_vertices[gl_VertexID], 0.0, 1.0);
-}
