d08aa29e63e900ff6e16ba849b6339a95f16a8d7
ClawmanCat <clawmancat@gmail.com>
2022-03-28 12:45:39 +0200

    vcvars

--- .github/workflows/run_cmake.py
+++ .github/workflows/run_cmake.py
@@ -44,7 +44,8 @@ def find(root_dir, file):
 
   
 def main():
-    print(f"vcvars64: {find('C:\\Program Files (x86)\\Microsoft Visual Studio', 'vcvars64.bat')}", flush = True)
+    where = find(R'C:\Program Files (x86)\Microsoft Visual Studio', 'vcvars64.bat')
+    print(f'vcvars64: {where}', flush = True)
 
     # runcmd(R'C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat')
     log_env()
